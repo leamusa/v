6d91f38c0d1c00b2523ec661e64f2ffe5df6a6e0
leandramusto@gmail.com <leandramusto@gmail.com>
2024-03-22 11:54:44 +0100

    add a main vue component; exporting main; add array of obj

---
+++ src/components/Main.vue
@@ -0,0 +1,132 @@
+<script>
+export default {
+  name: "Main",
+  components: {
+    ProductCard,
+  },
+  data() {
+    return {
+      card: [
+        {
+          id: 1,
+          frontImage: "1.webp",
+          backImage: "1b.webp",
+          brand: "Levi's",
+          name: "Relaxed Fit",
+          price: 29.99,
+          isInFavorites: true,
+          badges: [
+            {
+              type: "tag",
+              value: "Sostenibilità",
+            },
+            {
+              type: "discount",
+              value: "-50%",
+            },
+          ],
+        },
+        {
+          id: 2,
+          frontImage: "2.webp",
+          backImage: "2b.webp",
+          brand: "Guess",
+          name: "Roses Tee",
+          price: 20.99,
+          isInFavorites: true,
+          badges: [
+            {
+              type: "discount",
+              value: "-30%",
+            },
+          ],
+        },
+        {
+          id: 3,
+          frontImage: "3.webp",
+          backImage: "3b.webp",
+          brand: "Come Zucchero Filato",
+          name: "Voglia di colori pastello",
+          price: 129.99,
+          isInFavorites: false,
+          badges: [
+            {
+              type: "discount",
+              value: "-30%",
+            },
+          ],
+        },
+        {
+          id: 4,
+          frontImage: "4.webp",
+          backImage: "4b.webp",
+          brand: "Levi's",
+          name: "Tee Unisex",
+          price: 14.99,
+          isInFavorites: false,
+          badges: [
+            {
+              type: "tag",
+              value: "Sostenibilità",
+            },
+            {
+              type: "discount",
+              value: "-50%",
+            },
+          ],
+        },
+        {
+          id: 5,
+          frontImage: "5.webp",
+          backImage: "5b.webp",
+          brand: "Maya Deluxe",
+          name: "Stripe Bodice",
+          price: 99.99,
+          isInFavorites: true,
+          badges: [
+            {
+              type: "tag",
+              value: "Sostenibilità",
+            },
+            {
+              type: "discount",
+              value: "-50%",
+            },
+          ],
+        },
+        {
+          id: 6,
+          frontImage: "6.webp",
+          backImage: "6b.webp",
+          brand: "Esprit",
+          name: "Maglione - Black",
+          price: 29.99,
+          isInFavorites: true,
+          badges: [
+            {
+              type: "tag",
+              value: "Sostenibilità",
+            },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="card" style="width: 18rem">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+      <p class="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </p>
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </div>
+  </div>
+</template>
+<style></style>
