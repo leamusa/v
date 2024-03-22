1ad2dfce2a3d5928aab0d6faf77952c7fbbf45cd
leandramusto@gmail.com <leandramusto@gmail.com>
2024-03-22 11:08:31 +0100

    importing packages vite and bootsptrap,making an header

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
+<template>
+  <Header />
+</template>
+
+<style>
+@import "bootstrap/dist/css/bootstrap.min.css";
+</style>
